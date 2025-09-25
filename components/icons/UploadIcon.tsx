21fe458f7171e63f2b7bb5fe69e706f8d45756fc
ketanvelip-scopely <ketan.velip@scopely.com>
2025-09-25 16:35:09 +0530

    restructure project

--- components/icons/UploadIcon.tsx
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const UploadIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l-3.75 3.75M12 9.75l3.75 3.75M3 17.25V21h18v-3.75" />
-  </svg>
-);
